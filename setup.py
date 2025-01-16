cf18c057d43e485df72e1c2c5bd45c7ce5d9e2cd
Richard Kubíček <rici.kubicek@gmail.com>
2025-01-16 14:23:32 +0100

    Update version to v0.1.0

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='superfaktura_client',
-    version='0.0.2',
+    version='0.1.0',
     packages=find_namespace_packages(),
     url='https://github.com/eledio-helpers/superfaktura-client',
-    license='',
+    license='GNU GPLv3',
     author='Richard Kubíček',
     author_email='kubicekr@eledio.com',
     install_requires=[
