36b09c911813618c5f32b7f2ffadc0e281fe6e73
Richard Kubíček <rici.kubicek@gmail.com>
2025-01-16 13:47:24 +0100

    project: add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='superfaktura_client',
+    version='1.0.0',
+    packages=find_namespace_packages(),
+    url='https://github.com/eledio-helpers/superfaktura-client',
+    license='',
+    author='Richard Kubíček',
+    author_email='kubicekr@eledio.com',
+    description=''
+)
