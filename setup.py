84f5cd8642f6975241b6b134fecba76480bce46e
Richard Kubíček <rici.kubicek@gmail.com>
2025-01-25 19:18:07 +0100

    setup: format

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='superfaktura_client',
-    version='0.1.0',
+    name="superfaktura_client",
+    version="0.1.0",
     packages=find_namespace_packages(),
-    url='https://github.com/eledio-helpers/superfaktura-client',
-    license='GNU GPLv3',
-    author='Richard Kubíček',
-    author_email='kubicekr@eledio.com',
-    install_requires=[
-        "requests",
-        "python-dotenv"
-    ],
+    url="https://github.com/eledio-helpers/superfaktura-client",
+    license="GNU GPLv3",
+    author="Richard Kubíček",
+    author_email="kubicekr@eledio.com",
+    install_requires=["requests", "python-dotenv"],
 )
