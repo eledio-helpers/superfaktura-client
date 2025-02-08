ef0974702f82e533f6f9384956d977569f699c57
Richard Kubíček <rici.kubicek@gmail.com>
2025-02-08 14:32:59 +0100

    currency: fix module by pylint hints

--- superfaktura/enumerations/currency.py
+++ superfaktura/enumerations/currency.py
@@ -1,3 +1,30 @@
+"""
+Currency Enumeration.
+
+This module provides an enumeration of currencies that can be used in the SuperFaktura API.
+
+Classes:
+    - Currencies: Enumeration of currencies.
+
+Usage:
+    from superfaktura.enumerations.currency import Currencies
+    currency = Currencies.CZK
+"""
+
+
 class Currencies:
+    """
+    Currency Enumeration.
+
+    This enumeration represents the different currencies that can be used in the SuperFaktura API.
+
+    Values:
+        - CZK: Czech Koruna
+        - EUR: Euro
+
+    Usage:
+        currency = Currencies.CZK
+    """
+
     CZK = "CZK"
     EUR = "EUR"
