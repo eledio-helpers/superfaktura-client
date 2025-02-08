3c5ea2e2e203d02c09d3460c9c70754663e1acc6
Richard Kubíček <rici.kubicek@gmail.com>
2025-02-08 14:37:02 +0100

    country: fix module by pylint hints

--- superfaktura/utils/country.py
+++ superfaktura/utils/country.py
@@ -1,12 +1,33 @@
-from pprint import pprint
+"""
+Country Module.
+
+This module provides utilities for working with countries in the SuperFaktura API.
+
+Functions:
+    - country_list: Retrieves a list of countries.
+
+Usage:
+    from superfaktura.utils.country import country_list
+    countries = country_list()
+    print(countries)
+"""
 
 from superfaktura.superfaktura_api import SuperFakturaAPI
 
 
 def country_list():
+    """
+    Retrieves a list of countries.
+
+    This function returns a list of countries that can be used in the SuperFaktura API.
+
+    Returns:
+        - list: A list of countries.
+
+    Usage:
+        countries = country_list()
+        print(countries)
+    """
     api = SuperFakturaAPI()
     url = "countries"
     return api.get(url)
-
-
-pprint(country_list())
