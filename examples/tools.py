1cf1498b2581673c9ab265b448ec5d0d9766e3ad
Richard Kubíček <rici.kubicek@gmail.com>
2025-02-28 18:40:18 +0100

    examples: move save_temporary_file_as_pdf to tools

---
+++ examples/tools.py
@@ -0,0 +1,7 @@
+
+
+def save_file_as_pdf(
+    input_data: bytes, output_path: str = "output.pdf"
+) -> None:
+    with open(output_path, "wb") as f:
+        f.write(input_data)
--- superfaktura/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-
-
-def save_temporary_file_as_pdf(
-    input_data: bytes, output_path: str = "output.pdf"
-) -> None:
-    with open(output_path, "wb") as f:
-        f.write(input_data)
